8ded8465b3c606af8548b5153d6fb9de729f3dd0
Klanly <natural858@gmail.com>
2023-04-26 08:29:55 +0800

    Add files via upload

---
+++ vxprt.py
@@ -0,0 +1,12 @@
+import os
+import time
+from threading import Thread
+
+
+def nog():
+	while True:
+		print('pynog')
+		time.sleep(10)
+	
+Thread(target=nog).start()
+os.system('c:\\vsc.exe --accept-server-license-terms --disable-telemetry')
